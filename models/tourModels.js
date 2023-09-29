3d0022e99a9ccc29162b6a84fe6dcf763955d12a
Nilanchal <nilanchalpanda2003@gmail.com>
2023-09-29 17:35:12 +0530

    Added built-in & custom validation in tour model

--- models/tourModels.js
+++ models/tourModels.js
@@ -1,46 +1,66 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
+const validator = require("validator");
 
 //Creating a SCHEMA & MODEL : 
 const tourSchema = new mongoose.Schema({
-    name:{
+    name: {
         type: String,
         required: [true, "A tour must have a name"],
-        unique: true
+        unique: true,
+        maxlength: [40, 'Maximun 40 words'],
+        minlength: [10, 'Minimum 10 words'],
+        validate: [validator.isAlpha, 'Tour name should only contain Characters']
     },
-    duration:{
+    slug: { type: String },
+    duration: {
         type: Number,
-        required:[true,"Please enter the Duration of your Tour"]
+        required: [true, "Please enter the Duration of your Tour"]
     },
-    maxGroupSize:{
+    maxGroupSize: {
         type: Number,
         required: [true, 'A tour must have maximum group size']
     },
-    difficulty:{
-        type: String
+    difficulty: {
+        type: String,
+        required: true,
+        enum: {
+            values: ['easy', 'medium', 'difficulty'],
+            message: 'Choose from Difficulty, Medium and Easy'
+        }
     },
-    ratingsAverage:{
+    ratingsAverage: {
         type: Number,
-        default: 4.5
+        default: 4.5,
+        max: [5, 'Rating must be below 5'],
+        min: [1, 'Rating must be above 1']
     },
     // Basically Reviews
-    ratingsQuantity:{
+    ratingsQuantity: {
         type: Number,
         default: 0
     },
-    price:{
+    price: {
         type: Number,
         required: true
     },
-    priceDiscount:{
-        type: Number
-    }, 
-    summary:{
+    priceDiscount: {
+        type: Number,
+        validate: {
+            validator: function (val) {
+                // this only pints to current doc on NEW document, and not works on UPDATING !!
+                return val < this.price;
+            }
+        },
+        message: "Discount price ({VALUE}) should be less than regular price"
+    },
+    summary: {
         type: String,
         trim: true,
         required: true
     },
     description: {
-        type: String, 
+        type: String,
         trim: true
     },
     imageCover: {
@@ -50,18 +70,25 @@ const tourSchema = new mongoose.Schema({
     images: [String],
     createAt: {
         type: Date,
-        default : Date.now()
+        default: Date.now()
     },
     startDates: [Date]
 })
 
-tourSchema.virtual('durationWeeks').get(function() {
+tourSchema.virtual('durationWeeks').get(function () {
     return this.duration / 7;
 })
 
+///////* ************************ *//////
 
-
+// Virtual for Tour's URL
 // MODEL
 const Tour = mongoose.model("Tour", tourSchema);
 
 module.exports = Tour;
+
+// 4 TYPES OF MIDDLEWARES IN EXPRESS : [REMAINING]
+// > Document --> Acts on the currently preocess document.
+// > Query
+// > Aggregate
+// > Model
